469d93ed35eed0ec619cb3bc645202d434d1cec3
Tanim <42853370+tanim913@users.noreply.github.com>
2022-06-17 05:54:49 +0600

    1

---
+++ DSA Cracker Sheet 450/1-reverse-the-array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >>n;
+  int ar [n];
+
+  for(int i =0; i<n; i++)
+  {
+    cin >> ar[i];
+  }
+  int left = 0;
+  int right = n-1;
+
+  while (left < right) {
+    swap(ar[left] , ar[right]);
+    left ++;
+    right --;
+  }
+  for(int i =0; i<n; i++)
+  {
+    cout << ar[i] <<  " ";
+  }
+  return 0;
+}
